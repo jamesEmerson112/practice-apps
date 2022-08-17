98581931799d289ce00897eae4db17ebca5f10fb
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-08-17 14:12:09 -0700

    Fixed states

--- 1-glossary/client/src/components/Entry.jsx
+++ 1-glossary/client/src/components/Entry.jsx
@@ -4,13 +4,46 @@ import { render } from "react-dom";
 class Entry extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      word: '',
+      definition: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    alert('A word was submitted: ' + this.state.word);
+    alert('A definition was submitted: ' + this.state.definition);
+    console.log(this.state);
+
+    event.preventDefault();
+  }
+
+
 
   render() {
     return (
       <div>
-        <form>
+        <form id="entry-form">
           <h3>Entry Form:</h3>
+          <input type="text" id="word" name="word"
+          placeholder="word"
+          onChange={this.handleChange}></input>
+          <br></br>
+          <input type="text" id="definition" name="definition"
+          placeholder="definition"
+           onChange={this.handleChange}></input>
+          <br></br>
+          <button className="entry-button" type="button" onClick={this.handleSubmit}>Submit</button>
         </form>
       </div>
     )
